cfec5cbdab03ba077cb0c0fc08bada07889ad98a
suvoajay <75306837+suvoajay@users.noreply.github.com>
2021-09-26 17:44:18 +0530

    Update dockerce.pp

--- dockerce.pp
+++ dockerce.pp
@@ -3,6 +3,6 @@ class { 'docker':
   manage_package              => true,
   use_upstream_package_source => false,
   package_engine_name         => 'docker-engine'
-  package_source_location     => 'https://download.docker.com/linux/centos/8/x86_64/nightly/Packages/containerd.io-1.4.9-3.1.el8.x86_64.rpm',
+  package_source_location     => https://download.docker.com/linux/centos/8/x86_64/nightly/Packages/containerd.io-1.4.9-3.1.el8.x86_64.rpm,
   prerequired_packages        => [ 'glibc.i686', 'glibc.x86_64', 'sqlite.i686', 'sqlite.x86_64', 'device-mapper', 'device-mapper-libs', 'device-mapper-event-libs', 'device-mapper-event' ]
 }
